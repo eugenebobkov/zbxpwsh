913545d6670d11ecdae5fbfbeb5b370b070f60f5
Eugene Bobkov <eugene.bobkov@gmail.com>
2019-02-08 16:37:59 +1030

    Added utility to generate encripted passwords

---
+++ bin/pwgen.ps1
@@ -0,0 +1,10 @@
+﻿Param (
+    [Parameter(Mandatory=$true, Position=1)][string]$InputString        # Password to encrypt
+)
+
+$RootPath = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$global:ScriptName = Split-Path -Leaf $MyInvocation.MyCommand.Definition
+
+Import-Module -Name "$global:RootPath\lib\Library-StringCrypto.psm1"
+
+Write-EncryptedString -InputString $InputString -Password (Get-Content "$global:RootPath\etc\.pwkey")
